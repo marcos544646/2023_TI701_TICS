6e2023fde118f4d50decd050b7f04f5d0326cbf5
marcos544646 <131731604+marcos544646@users.noreply.github.com>
2023-09-22 03:31:38 -0600

    FIRST COMMIT

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*
+ * ITESS TICS
+   *TALLER DE INVESTIGACION
+*intento de curso de certificacion java 0Z1-808
+* By MMGN
+* 22/Septiembre/2023
+ */
+
+/**
+ *
+ * @author marc0
+ */
+public class Main {
+    
+}
